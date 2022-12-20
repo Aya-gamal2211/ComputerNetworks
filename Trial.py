b9e52cef75a6f7fb7da6616012007ece0fa5504b
Aya-gamal2211 <ayaahmadgamal2000@gmail.com>
2022-12-20 13:58:28 +0200

    Update Trial.py

--- Trial.py
+++ Trial.py
@@ -15,7 +15,8 @@ while True:
     (clientsocket, address) = tcpSerSock.accept()
 
 # Fill in end.
-while 1:
+
+
     # Start receiving data from the client
     print ('\n\nReady to serve...')
     tcpCliSock, addr = tcpSerSock.accept()
@@ -25,12 +26,12 @@ while 1:
     # Fill in end.
     print (message)
     # Extract the filename from the given message
-    print message.split()[1]
+    print (message.split()[1])
     filename = message.split()[1].partition("/")[2]
-    print filename
+    print (filename)
     fileExist = "false"
     filetouse = "/" + filename
-    print filetouse
+    print (filetouse)
     try:
         # Check wether the file exist in the cache
         f = open(filetouse[1:], "r")
@@ -53,29 +54,36 @@ while 1:
             c = socket(AF_INET, SOCK_STREAM)
             # Fill in end.
             hostn = filename.replace("www.","",1)
-            print hostn
+            print (hostn)
             try:
                 # Connect to the socket to port 80
                 # Fill in start.
+                c.connect((hostn,80))
+                print("Connected to port 80")
                 # Fill in end.
                 # Create a temporary file on this socket and ask port 80 for the file requested by the client
                 fileobj = c.makefile('r', 0)
                 fileobj.write("GET "+"http://" + filename + " HTTP/1.0\n\n")
 # Read the response into buffer
 # Fill in start.
+                buffer=fileobj.readlines()
 # Fill in end.
 # Create a new file in the cache for the requested file.
 # Also send the response in the buffer to client socket and the corresponding file in the cache
                 tmpFile = open("./" + filename,"wb")
-                # Fill in start.
+                # Fill in start.\
+                for i in range(0, len(buffer)):
+                    tmpFile.write(buffer[i])
+                    tcpCliSock.send(buffer[i])
                 # Fill in end.
             except:
                 print ("Illegal request")
         else:
             # HTTP response message for file not found
             # Fill in start.
+            
             # Fill in end.
-            # Close the client and the server sockets
+# Close the client and the server socket
             tcpCliSock.close()
 # Fill in start.
 # Fill in end
